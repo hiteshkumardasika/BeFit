237589af31c839d96d058b97cb4e764cf02ecb0a
jeffrey <jeffravi@iu.edu>
2017-04-19 09:21:08 -0400

    Added Rewards layout

---
+++ app/src/main/java/com/example/root/befit/reward/RewardDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.example.root.befit.reward;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.root.befit.R;
+
+public class RewardDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reward_details);
+    }
+}
